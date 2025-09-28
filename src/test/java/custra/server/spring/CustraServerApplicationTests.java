94941d880c7b3f34aad9ca94e8ffa74c30a3df9b
MatinDev <Mohammad.Matin.k.1384@Gmail.com>
2025-09-28 21:26:11 +0330

    Adding remaining files

---
+++ src/test/java/custra/server/spring/CustraServerApplicationTests.java
@@ -0,0 +1,13 @@
+package custra.server.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustraServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
